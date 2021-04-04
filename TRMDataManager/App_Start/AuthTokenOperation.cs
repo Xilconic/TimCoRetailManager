dab146723949f8240e704d21a6c946cef58c3cb3
xilconic <xilconic@gmail.com>
2021-04-04 09:14:46 +0200

    Add workaround for authentication and providing auth-token in Swagger UI

---
+++ TRMDataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Web.Http.Description;
+using Swashbuckle.Swagger;
+
+namespace TRMDataManager.App_Start
+{
+    /// <summary>Adds a page to get a authentication token from the Swagger UI.</summary>
+    /// <remarks>
+    /// source: https://stackoverflow.com/questions/51117655
+    /// </remarks>
+    /// <seealso cref="AuthorizationOperationFilter"/>
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation
+                {
+                    tags = new List<string>{ "Auth" },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = true,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = true,
+                            @in = "formData"
+                        },
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ TRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Web.Http.Description;
+using Swashbuckle.Swagger;
+
+namespace TRMDataManager.App_Start
+{
+    /// <summary>Extend all documented endpoints with ability to provided auth token.</summary>
+    /// <remarks>
+    /// source: https://stackoverflow.com/questions/51117655
+    /// </remarks>
+    /// <seealso cref="AuthTokenOperation"/>
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "Provide the authorization headers. Remember to add \"bearer \" in front of your token.",
+                required = false,
+                type = "string",
+            });
+        }
+    }
+}
