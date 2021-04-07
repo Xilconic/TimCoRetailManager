52814babae9e0125b5c6b595c2c1b2753c9fdc40
xilconic <xilconic@gmail.com>
2021-04-07 19:55:47 +0200

    Define skeleton SalesView(Model) scaffolding
    Not actual behavior implemented yet.
    For more details: https://www.youtube.com/watch?v=I0M2ne00s5A

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,121 @@
+﻿using System.ComponentModel;
+using Caliburn.Micro;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+        private BindingList<string> _cart;
+        private string _itemQuantity;
+
+        public BindingList<string> Products
+        {
+            get => _products;
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(nameof(Products));
+            }
+        }
+
+        public BindingList<string> Cart
+        {
+            get => _cart;
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(nameof(Cart));
+            }
+        }
+
+        public string ItemQuantity
+        {
+            get => _itemQuantity;
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(nameof(ItemQuantity));
+            }
+        }
+
+        public string SubTotal
+        {
+            get
+            {
+                // TODO: replace with calculation
+                return $"0.00";
+            }
+        }
+
+        public string Tax
+        {
+            get
+            {
+                // TODO: replace with calculation
+                return $"0.00";
+            }
+        }
+
+        public string Total
+        {
+            get
+            {
+                // TODO: replace with calculation
+                return $"0.00";
+            }
+        }
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure something is selected
+                // Make sure there is an item quantity
+
+                return output;
+            }
+        }
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure something is selected
+
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure something is the cart
+
+                return output;
+            }
+        }
+
+        public void CheckOut()
+        {
+
+        }
+    }
+}
